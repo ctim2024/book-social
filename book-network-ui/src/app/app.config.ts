6f9236294b569b845671ef104a9b82e96aac7338
boubaker <belgecemjrad2018@gmail.com>
2024-12-10 10:32:57 +0200

    modify app.config.ts

--- book-network-ui/src/app/app.config.ts
+++ book-network-ui/src/app/app.config.ts
@@ -4,7 +4,21 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { httpTokenInterceptor } from './services/interceptor/http-token.interceptor';
+import { ApiModule } from './services/api.module';
+import { ApiConfiguration } from './services/api-configuration';
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(withInterceptors([httpTokenInterceptor]))]
+
+  
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
+              provideRouter(routes), 
+              provideHttpClient(withInterceptors([httpTokenInterceptor])),
+                // Provide ApiModule with configuration
+    {
+      provide: ApiConfiguration,
+      useValue: {
+        rootUrl: 'http://10.76.100.239:8088/api/v1', // Update this to your API URL
+      },
+    }]
 };
